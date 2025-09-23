0c72eb4c011c373d9eb9089d1a3eba2bc6740ac2
syedtalhahamid <syedtalhacodenera@gmail.com>
2025-09-23 20:42:17 +0530

    intial

--- app.py
+++ app.py
@@ -14,12 +14,15 @@ def post():
 def poster():
     return render_template("index.html")
 
+
 if __name__=="__main__":
     app.run(host="127.0.0.1",port=5000)
 
 
 # Keep your local changes (ignore incoming):
 # git merge --strategy-option ours feature
+# git merge -X ours dev
 
 # Keep their branch changes (ignore local):
 # git merge --strategy-option theirs feature
+# git merge -X theirs dev
