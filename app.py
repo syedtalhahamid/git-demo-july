1a7ec70335b222bf89e8801b5633e96564718163
syedtalhahamid <syedtalhacodenera@gmail.com>
2025-09-23 21:49:24 +0530

    intial commit

--- app.py
+++ app.py
@@ -18,6 +18,11 @@ def poster():
 def add_post():
     return render_template("index.html")
 
+@app.route('/post')
+def update_post():
+    return render_template("index.html")
+
 if __name__=="__main__":
     app.run(host="127.0.0.1",port=5000)
 
+#  git checkout --theirs app.py
