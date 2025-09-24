9c28234d44106af224de6a1caaa7d2ec45f5797e
syedtalhahamid <syedtalhacodenera@gmail.com>
2025-09-24 18:31:06 +0530

    initial commit

--- app.py
+++ app.py
@@ -6,22 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/post')
-def post():
-    return render_template("index.html")
-
-@app.route('/post/id')
-def poster():
-    return render_template("index.html")
-
-@app.route('/post')
-def add_post():
-    return render_template("index.html")
-
-@app.route('/post')
-def update_post():
-    return render_template("index.html")
-
 if __name__=="__main__":
     app.run(host="127.0.0.1",port=5000)
 
