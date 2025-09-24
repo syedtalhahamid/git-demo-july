8191bd08b167e0120d4d92d3b1b5a6dda35f825b
syedtalhahamid <syedtalhacodenera@gmail.com>
2025-09-24 18:34:11 +0530

    initial commit

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
 if __name__=="__main__":
     app.run(host="127.0.0.1",port=5000)
 
